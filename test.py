8cf67e530b232ffed2f42952d2ec5ad54706aec7
Rawan Emish <rawanemish@gmail.com>
2026-04-22 10:45:57 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+store = {
+    'Seafood':{'Small':200, 'Medium':300},
+    'Pasta':{'Small':200, 'Medium':300},
+    'Water':{'Small':200, 'Medium':300}
+}
+store['Steak'] = 200
+
+print(store['Seafood']['Small'])
+
